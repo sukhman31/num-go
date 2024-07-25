bcd39fb06c43cfd6585a4d359523ad85c63cbabd
sukhman31 <labana.sukhman@gmail.com>
2024-07-26 00:19:25 +0530

    formatting code

--- num-go/internal/array/ndimarray_test.go
+++ num-go/internal/array/ndimarray_test.go
@@ -8,17 +8,17 @@ import (
 
 func Test_NewArray(t *testing.T) {
 	testCases := []struct {
-		data []float64
-		shape []int
-		want *Array
+		data        []float64
+		shape       []int
+		want        *Array
 		expectedErr bool
 	}{
 		{
 			[]float64{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12},
 			[]int{2, 3, 2},
 			&Array{
-				data: []float64{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12},
-				shape: []int{2, 3, 2},
+				data:    []float64{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12},
+				shape:   []int{2, 3, 2},
 				strides: []int{6, 2, 1},
 			},
 			false,
@@ -36,18 +36,18 @@ func Test_NewArray(t *testing.T) {
 
 func Test_At(t *testing.T) {
 	testCases := []struct {
-		array *Array
-		position []int
-		want float64
+		array       *Array
+		position    []int
+		want        float64
 		expectedErr bool
 	}{
 		{
 			&Array{
-				data: []float64{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12},
-				shape: []int{2, 3, 2},
+				data:    []float64{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12},
+				shape:   []int{2, 3, 2},
 				strides: []int{6, 2, 1},
 			},
-			[]int{0,1,0},
+			[]int{0, 1, 0},
 			float64(3),
 			false,
 		},
